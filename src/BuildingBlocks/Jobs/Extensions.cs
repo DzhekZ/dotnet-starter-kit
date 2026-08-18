a1281630fbb5c6b5e1f206076fb9f6f2ed5884bc
Eugeny Zebrov <ezebrov@gmail.com>
2026-08-18 12:03:08 +0300

    merge

--- src/BuildingBlocks/Jobs/Extensions.cs
+++ src/BuildingBlocks/Jobs/Extensions.cs
@@ -79,8 +79,8 @@ public static class Extensions
             [
                new HangfireCustomBasicAuthenticationFilter
                {
-                    User = hangfireOptions.UserName!,
-                    Pass = hangfireOptions.Password!
+                    User = hangfireOptions.UserName,
+                    Pass = hangfireOptions.Password
                }
             ]
         };
